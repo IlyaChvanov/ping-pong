04c55ed29214e11c7aeeb1db520e54781b30e097
ilya <ivchvanov@edu.hse.ru>
2024-11-27 19:23:23 +0300

    C++ style and waiting for a second

--- server.cpp
+++ server.cpp
@@ -13,64 +13,58 @@
 
 int main() {
     char buffer[BUFFER_SIZE];
-
     if (!std::filesystem::exists(FIFO_PATH)) {
-        mkfifo(FIFO_PATH, 0666);
+        if (mkfifo(FIFO_PATH, 0666) == -1 && errno != EEXIST) {
+            std::cerr << "Could not create FIFO" << std::endl;
+            return -1;
+        }
     }
 
     while (true) {
-        // Ожидание сообщений от клиента
+        std::this_thread::sleep_for(std::chrono::seconds(1));
         int fifo_fd = open(FIFO_PATH, O_RDONLY);
         if (fifo_fd == -1) {
-            perror("Error while opening FIFO for reading");
+            std::cerr << "Error while opening FIFO for reading" << std::endl;
             exit(1);
         }
-
         ssize_t bytes_read = read(fifo_fd, buffer, BUFFER_SIZE - 1);
         if (bytes_read == -1) {
-            perror("Error while reading from FIFO");
+            std::cerr << "Error while reading from FIFO" << std::endl;
             close(fifo_fd);
             exit(1);
         }
-
         if (bytes_read == 0) {
-            printf("FIFO is empty, waiting for data...\n");
+            std::cout << "FIFO is empty, waiting for data..." << std::endl;
             close(fifo_fd);
             continue;
         }
 
         buffer[bytes_read] = '\0';
-        std::cout << "Received request: " << buffer;
-
-        try {
-            if (buffer == std::string("ping")) {
-                int fifo_fd_write = open(FIFO_PATH, O_WRONLY);
-                if (fifo_fd_write == -1) {
-                    perror("Error while opening FIFO for writing");
-                    close(fifo_fd);
-                    exit(1);
-                }
-
-                const char *response = "pong";
-                ssize_t bytes_written = write(fifo_fd_write, response, strlen(response));
-                if (bytes_written == -1) {
-                    perror("Error while writing to FIFO");
-                    close(fifo_fd);
-                    close(fifo_fd_write);
-                    exit(1);
-                }
-
-                printf("Sent: \"%s\"\n", response);
-                close(fifo_fd_write);
-            } else {
-                throw std::runtime_error("message is not ping");
+        std::cout << "Received request: " << buffer << std::endl;
+        if (buffer == std::string("ping")) {
+            int fifo_fd_write = open(FIFO_PATH, O_WRONLY);
+            if (fifo_fd_write == -1) {
+                std::cerr << "Error while opening FIFO for writing" << std::endl;
+                close(fifo_fd);
+                exit(1);
             }
-        } catch (const std::exception &e) {
-            std::cout << e.what() << std::endl;
+            const char *response = "pong";
+            ssize_t bytes_written = write(fifo_fd_write, response, strlen(response));
+            if (bytes_written == -1) {
+                std::cerr << "Error while writing to FIFO" << std::endl;
+                close(fifo_fd);
+                close(fifo_fd_write);
+                exit(1);
+            }
+            std::cout << "Sent: \"" << response << "\"" << std::endl;
+            close(fifo_fd_write);
+        } else {
+            std::cerr << "Error: the message is not 'ping'" << std::endl;
+            close(fifo_fd);
             continue;
         }
+
         close(fifo_fd);
     }
-
     return 0;
 }
