6101dba8bdf0079b3fd4c1786981617a4870ab08
ilya <ivchvanov@edu.hse.ru>
2024-12-01 22:05:53 +0300

    improved server.cpp

--- server.cpp
+++ server.cpp
@@ -1,15 +1,15 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <cstdio>
+#include <cstdlib>
 #include <unistd.h>
 #include <fcntl.h>
 #include <filesystem>
 #include <iostream>
-#include <string.h>
+#include <cstring>
 #include <thread>
 #include <sys/stat.h>
 
 #define FIFO_PATH "/tmp/pingpong_fifo"
-#define BUFFER_SIZE 1024
+#define BUFFER_SIZE 5
 
 // !!ДЛЯ ДИМЫ!!
 // Файл автоматически очищается при чтении данных, так что, когда будешь делать клиента, сразу считывай
@@ -25,26 +25,24 @@ int main() {
     }
 
     while (true) {
-        std::this_thread::sleep_for(std::chrono::seconds(1));
         int fifo_fd = open(FIFO_PATH, O_RDONLY);
         if (fifo_fd == -1) {
             std::cerr << "Error while opening FIFO for reading" << std::endl;
             exit(1);
         }
         ssize_t bytes_read = read(fifo_fd, buffer, BUFFER_SIZE - 1);
+        close(fifo_fd);
         if (bytes_read == -1) {
             std::cerr << "Error while reading from FIFO" << std::endl;
-            close(fifo_fd);
             exit(1);
         }
         if (bytes_read == 0) {
             std::cout << "FIFO is empty, waiting for data..." << std::endl;
-            close(fifo_fd);
             continue;
         }
 
         buffer[bytes_read] = '\0';
-        std::cout << "Received request: " << buffer << std::endl;
+        std::cout << "Server Received: " << buffer << std::endl;
         if (buffer == std::string("ping")) {
             int fifo_fd_write = open(FIFO_PATH, O_WRONLY);
             if (fifo_fd_write == -1) {
@@ -54,21 +52,19 @@ int main() {
             }
             const char *response = "pong";
             ssize_t bytes_written = write(fifo_fd_write, response, strlen(response));
+            close(fifo_fd_write);
+
             if (bytes_written == -1) {
                 std::cerr << "Error while writing to FIFO" << std::endl;
                 close(fifo_fd);
                 close(fifo_fd_write);
                 exit(1);
             }
-            std::cout << "Sent: \"" << response << "\"" << std::endl;
-            close(fifo_fd_write);
+            std::cout << "Server Sent: \"" << response << "\"" << std::endl;
         } else {
             std::cerr << "Error: the message is not 'ping'" << std::endl;
             close(fifo_fd);
-            continue;
         }
-
-        close(fifo_fd);
     }
     return 0;
 }
